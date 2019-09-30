83df7d6aabb046e151489fadb0be627a7dc8f0da
Barbara Muesing <muesingb@gmail.com>
2019-09-30 22:44:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 def map(source_array)
 i = 0
   return_array = []
-    while i < source_array.length do
-      return_array[i] = yield(source_array[i])
-      i += 1
-    end
+  while i < source_array.length do
+    return_array[i] = yield(source_array[i])
+    i += 1
+  end
   return return_array
 end
 
